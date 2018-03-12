5393ccbfb4a6ed8bc41f8751a78010eb17d5ca81
Allison Bolen <Allybolen629@gmail.com>
2018-03-12 17:56:33 -0400

    Update observable.py

--- observable.py
+++ observable.py
@@ -1,10 +1,15 @@
 class Observable(object):
-    def __init__(self, name):
-        self.subscribers = set()
-    def register(self, who):
-        self.subscribers.add(who)
-    def unregister(self, who):
-        self.subscribers.discard(who)
-    def dispatch(self, message):
-        for subscriber in self.subscribers:
-            subscriber.update(message)
+ 
+        def __init__(self):    
+                self.observers = []
+ 
+        def add_observer(self, observer):
+                if not observer in self.observers:
+                        self.observers.append(observer)
+ 
+        def remove_observe(self, observer):
+                if observer in self.observers:
+                        self.observers.remove(observer)
+ 
+        def remove_all_observers(self):
+                self.observers = []
